96ead53c30b8f688d10f0e1403da2912bf59f928
davidGBbr <davidgomesbrigido@gmail.com>
2023-12-16 23:06:30 -0300

    header created

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = () => {
+  return <div>Página dashboard</div>;
+};
+
+export default Dashboard;
---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>Página login</div>;
+};
+
+export default Login;
